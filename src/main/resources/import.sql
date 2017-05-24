9e2d4c7977c6d7e60829de99011c3cf2bf268f69
kindcao <kind790@163.com>
2017-05-24 12:12:23 +0800

    MVC demo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,19 +25,56 @@ CREATE TABLE `hotel` (
 -- ----------------------------
 -- Records
 -- ----------------------------
-
-insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
-insert into city (name, state, country) values ('Montgomery', 'AL', 'US');
-insert into city (name, state, country) values ('Juneau', 'AK', 'US');
-insert into city (name, state, country) values ('Sacramento', 'CA', 'US');
-insert into city (name, state, country) values ('Tallahassee', 'FL', 'US');
-insert into city (name, state, country) values ('Honolulu', 'HI', 'US');
-insert into city (name, state, country) values ('Helena', 'MT', 'US');
-insert into city (name, state, country) values ('Harrisburg', 'PA', 'US');
-insert into city (name, state, country) values ('Salt Lake City', 'UT', 'US');
-insert into city (name, state, country) values ('Columbia', 'SC', 'US');
-insert into city (name, state, country) values ('Lincoln', 'NE', 'US');
-insert into city (name, state, country) values ('Jefferson City', 'MO', 'US');
+insert into city (name, state, country) values ('Montgomery','AL','US');
+insert into city (name, state, country) values ('Juneau','AK','US');
+insert into city (name, state, country) values ('Phoenix','AZ','US');
+insert into city (name, state, country) values ('Little rock','AR','US');
+insert into city (name, state, country) values ('Sacramento','CA','US');
+insert into city (name, state, country) values ('Denver','CO','US');
+insert into city (name, state, country) values ('Hartford','CT','US');
+insert into city (name, state, country) values ('Dover','DE','US');
+insert into city (name, state, country) values ('Tallahassee','FL','US');
+insert into city (name, state, country) values ('Atlanta','GA','US');
+insert into city (name, state, country) values ('Honolulu','HI','US');
+insert into city (name, state, country) values ('Boise','ID','US');
+insert into city (name, state, country) values ('Springfield','IL','US');
+insert into city (name, state, country) values ('Indianapolis','IN','US');
+insert into city (name, state, country) values ('Des Moines','IA','US');
+insert into city (name, state, country) values ('Topeka','KS','US');
+insert into city (name, state, country) values ('Frankfort','KY','US');
+insert into city (name, state, country) values ('Baton Rouge','LA','US');
+insert into city (name, state, country) values ('Augusta','ME','US');
+insert into city (name, state, country) values ('Annapolis','MD','US');
+insert into city (name, state, country) values ('Boston','MA','US');
+insert into city (name, state, country) values ('Lansing','MI','US');
+insert into city (name, state, country) values ('St. Paul','MN','US');
+insert into city (name, state, country) values ('Jackson','MS','US');
+insert into city (name, state, country) values ('Jefferson City','MO','US');
+insert into city (name, state, country) values ('Helena','MT','US');
+insert into city (name, state, country) values ('Lincoln','NE','US');
+insert into city (name, state, country) values ('Carson City','NV','US');
+insert into city (name, state, country) values ('Concord','NH','US');
+insert into city (name, state, country) values ('Trenton','NJ','US');
+insert into city (name, state, country) values ('Santa Fe','NM','US');
+insert into city (name, state, country) values ('Albany','NY','US');
+insert into city (name, state, country) values ('Raleigh','NC','US');
+insert into city (name, state, country) values ('Bismarck','ND','US');
+insert into city (name, state, country) values ('Columbus','OH','US');
+insert into city (name, state, country) values ('Oklahoma City','OK','US');
+insert into city (name, state, country) values ('Salem','OR','US');
+insert into city (name, state, country) values ('Harrisburg','PA','US');
+insert into city (name, state, country) values ('Providence','RI','US');
+insert into city (name, state, country) values ('Columbia','SC','US');
+insert into city (name, state, country) values ('Pierre','SD','US');
+insert into city (name, state, country) values ('Nashville','TN','US');
+insert into city (name, state, country) values ('Austin','TX','US');
+insert into city (name, state, country) values ('Salt Lake City','UT','US');
+insert into city (name, state, country) values ('Montpelier','VT','US');
+insert into city (name, state, country) values ('Richmond','VA','US');
+insert into city (name, state, country) values ('Olympia','WA','US');
+insert into city (name, state, country) values ('Charleston','WV','US');
+insert into city (name, state, country) values ('Madison','WI','US');
+insert into city (name, state, country) values ('Cheyenne','WY','US');
 
 -- ----------------------------
 -- Records
