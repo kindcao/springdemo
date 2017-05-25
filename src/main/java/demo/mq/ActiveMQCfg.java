8218cf09c604e49375e0d2876f688a896ce2ed4c
kindcao <kind790@163.com>
2017-05-25 14:49:02 +0800

    ActiveMQ demo

---
+++ src/main/java/demo/mq/ActiveMQCfg.java
@@ -0,0 +1,41 @@
+package demo.mq;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+/**
+ * Created by kind on 2017/5/24.
+ */
+@Configuration
+@EnableJms
+public class ActiveMQCfg {
+
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue("sample.queue");
+    }
+
+    @Bean
+    public Queue queueOut() {
+        return new ActiveMQQueue("sample.queue.out");
+    }
+
+    @Bean
+    public Topic topic() {
+        return new ActiveMQTopic("sample.topic");
+    }
+    //    @Bean
+    //    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+    //        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
+    //                ActiveMQConnectionFactory.DEFAULT_USER, ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+    //                //     "tcp://192.168.10.10:61616");
+    //                ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+    //        return activeMQConnectionFactory;
+    //    }
+}
---
+++ src/main/java/demo/mq/MqSampleApp.java
@@ -0,0 +1,15 @@
+package demo.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kind on 2017/5/24.
+ */
+@SpringBootApplication
+public class MqSampleApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MqSampleApp.class, args);
+    }
+}
